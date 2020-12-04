c7ba8d9e2eb46f41892e9c21ff5b12e1ccf7c68a
sim <sim@terra.money>
2020-12-05 01:04:43 +0900

    feature: Show price and total value on data page

--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -1,28 +1,53 @@
 import { useRecoilValue } from "recoil"
+import { div, plus, times } from "../libs/math"
 import { format } from "../libs/parse"
 import Page from "../components/Page"
 import Card from "../components/Card"
 import WithSuspense from "../containers/WithSuspense"
-import TextLoading from "../containers/TextLoading"
 import { poolQuery } from "../database/selectors"
 import { useListed } from "../database/useWhitelist"
 import styles from "./Data.module.scss"
 
+const Heading = () => (
+  <article className={styles.item}>
+    <h1 className={styles.title}>Symbol</h1>
+    <section className={styles.content}>UST</section>
+    <section className={styles.content}>Asset</section>
+    <section className={styles.content}>Price</section>
+    <section className={styles.content}>Total</section>
+  </article>
+)
+
 const Item = ({ token, symbol }: ListedItem) => {
   const pool = useRecoilValue(poolQuery(token))
+
+  const ust = pool?.ust
+  const asset = pool?.asset
+  const price = div(ust, asset)
+  const total = plus(ust, times(asset, price))
 
   return (
     <article className={styles.item}>
       <h1 className={styles.title}>{symbol}</h1>
       <section className={styles.content}>
-        {format(pool?.ust, undefined, { integer: true })} (UST)
+        {format(ust, undefined, { integer: true })}
       </section>
       <section className={styles.content}>
-        {format(pool?.asset, undefined, { integer: true })} (Asset)
+        {format(asset, undefined, { integer: true })}
+      </section>
+      <section className={styles.content}>{format(price)}</section>
+      <section className={styles.content}>
+        {format(total, undefined, { integer: true })}
       </section>
     </article>
   )
 }
+
+const Fallback = ({ symbol }: { symbol: string }) => (
+  <article className={styles.item}>
+    <h1 className={styles.title}>{symbol}</h1>
+  </article>
+)
 
 const Data = () => {
   const listed = useListed()
@@ -30,11 +55,20 @@ const Data = () => {
   return (
     <Page>
       <Card title="Pool">
-        {listed.map((item) => (
-          <WithSuspense fallback={<TextLoading />} key={item.token}>
-            <Item {...item} />
-          </WithSuspense>
-        ))}
+        <Heading />
+
+        {listed.map((item) => {
+          const { token, symbol, lp, pool } = item
+          const isToken = lp && pool
+
+          return (
+            isToken && (
+              <WithSuspense fallback={<Fallback symbol={symbol} />} key={token}>
+                <Item {...item} />
+              </WithSuspense>
+            )
+          )
+        })}
       </Card>
     </Page>
   )
