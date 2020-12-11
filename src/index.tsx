99d0bbc12cc6b0918b3cb329aa992008fdd64366
Paul Kim <paul@terra.money>
2020-12-11 19:17:20 +0900

    fix: disable sentry for now

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,17 @@ import { Integrations } from "@sentry/tracing"
 import "./index.scss"
 import App from "./App"
 
-process.env.NODE_ENV === "production" &&
-  Sentry.init({
-    dsn:
-      "https://50708db7737a4e0aa64b5f74890b2c47@o247107.ingest.sentry.io/5541003",
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
-    whitelistUrls: [
-      /^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.){0,1}mirror\.finance/,
-      /^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.){0,1}vercel\.app/,
-    ],
-  })
+// process.env.NODE_ENV === "production" &&
+//   Sentry.init({
+//     dsn:
+//       "https://50708db7737a4e0aa64b5f74890b2c47@o247107.ingest.sentry.io/5541003",
+//     integrations: [new Integrations.BrowserTracing()],
+//     tracesSampleRate: 1.0,
+//     whitelistUrls: [
+//       /^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.){0,1}mirror\.finance/,
+//       /^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.){0,1}vercel\.app/,
+//     ],
+//   })
 
 render(
   <StrictMode>
