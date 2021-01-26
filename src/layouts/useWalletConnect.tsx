10ec2820b9547c08780f9adc86c5939d5478ac11
sim <sim@terra.money>
2021-01-26 15:52:10 +0900

    feature: Set rpc url for WalletConnect

--- src/layouts/useWalletConnect.tsx
+++ src/layouts/useWalletConnect.tsx
@@ -1,14 +1,16 @@
 import { useSetRecoilState } from "recoil"
 import { ethers } from "ethers"
 import WalletConnectProvider from "@walletconnect/web3-provider"
-import { addressState, infuraId, providerState } from "../database/atoms"
+import { addressState, providerState } from "../database/atoms"
+
+const rpc = { 56: "https://bsc-dataseed.binance.org/" }
 
 const useWalletConnect = () => {
   const setProvider = useSetRecoilState(providerState)
   const setAddress = useSetRecoilState(addressState)
 
   const onClick = async () => {
-    const provider = new WalletConnectProvider({ infuraId })
+    const provider = new WalletConnectProvider({ rpc })
     const [address] = await provider.enable()
     setAddress(address)
     setProvider(new ethers.providers.Web3Provider(provider))
