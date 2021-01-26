a0935f53e56a9c0b94a9c2d04877a1ba86bc006d
sim <sim@terra.money>
2021-01-26 16:18:07 +0900

    fix: Redirect to my page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
-import { Route, Switch } from "react-router-dom"
+import { Redirect, Route, Switch } from "react-router-dom"
 import My from "./pages/my/My"
 import Send from "./pages/Send"
 
 export default (
   <Switch>
-    <Route path="/" exact component={My} />
+    <Route path="/my" component={My} />
     <Route path="/send/:token" component={Send} />
+    <Redirect to="/my" />
   </Switch>
 )
