3b373fac1ba394d3a76cd9049210fd6515fd4c04
sim <sim@terra.money>
2021-07-28 14:42:41 +0900

    Link to uniswap.rip instead of app.uniswap.org

--- src/ethereum/uniswap.ts
+++ src/ethereum/uniswap.ts
@@ -1,4 +1,4 @@
-const UNISWAP = "https://app.uniswap.org"
+const UNISWAP = "https://uniswap.rip"
 
 const getLinks = ({ input, output }: { input: string; output: string }) => ({
   swap: `${UNISWAP}/#/swap?inputCurrency=${input}&outputCurrency=${output}&use=V2`,
